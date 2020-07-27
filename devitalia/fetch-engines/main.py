3273dde04273ef53660e6e779b40b8ccb03b0480
Alessandro Sebastiani <sebbalex@users.noreply.github.com>
2020-07-27 16:11:44 +0200

    disable slack #7

--- devitalia/fetch-engines/main.py
+++ devitalia/fetch-engines/main.py
@@ -4,7 +4,7 @@ import argparse
 import engines
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-enabled_engines = ['GitHub', 'Slack', 'Forum', 'GAnalytics', 'Onboarding', 'Catalogo', 'CatalogoRegioni', 'CatalogoCategories', 'CatalogoAudiences']
+enabled_engines = ['GitHub', 'Forum', 'GAnalytics', 'Onboarding', 'Catalogo', 'CatalogoRegioni', 'CatalogoCategories', 'CatalogoAudiences']
 
 def compute_stats(args):
     for engine_name in enabled_engines:
